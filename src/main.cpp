5a3c554412656f978b0fd6a561f725a4e7c1bc9f
Ryoichi Ishikawa <ishikawa@cvl.iis.u-tokyo.ac.jp>
2020-07-17 16:46:15 +0900

    added comment, removed reflectance modification

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,8 @@ int main(int argv, char* argc[]) {
 	BasicPly bp;
 	bp.readPlyFile_(argc[1]);
 	
+	//visual position, visual orientation, threshold angle for projection (degree), in/output point cloud, threshold for jumpedge cutting
 	meshGenerater(Eigen::Vector3d::Zero(), Eigen::Matrix3d::Identity(),180, bp, 0.3);
-	std::vector<float> refl(bp.getVertexNumber(), 0.5);
-	bp.setReflectancePointer(refl.data(), bp.getVertexNumber());
 	bp.writePlyFileAuto(argc[2]);
 
 	return 0;
