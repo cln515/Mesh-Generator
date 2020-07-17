082f85f18ffbca5edb237b31a0d0434f7fd8f9de
Ryoichi Ishikawa <ishikawa@cvl.iis.u-tokyo.ac.jp>
2020-07-17 15:33:56 +0900

    initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <basicPly/BasicPly.h>
+#include <image_utility/image_utility.h>
 
 int main(int argv, char* argc[]) {
 	std::cout << "Kitty on your lap!" << std::endl;
+	
+	BasicPly bp;
+	bp.readPlyFile_(argc[1]);
+	
+	meshGenerater(Eigen::Vector3d::Zero(), Eigen::Matrix3d::Identity(),180, bp, 0.3);
+	std::vector<float> refl(bp.getVertexNumber(), 0.5);
+	bp.setReflectancePointer(refl.data(), bp.getVertexNumber());
+	bp.writePlyFileAuto(argc[2]);
+
 	return 0;
 }
